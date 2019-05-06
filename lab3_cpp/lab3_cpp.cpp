18240ad546dd2f670b991aa15eedbc691753896f
Mixa Penchev <mixa.penchev@gmail.com>
2019-05-06 23:30:49 +0300

    Добавьте файлы проекта.

---
+++ lab3_cpp/lab3_cpp.cpp
@@ -0,0 +1,16 @@
+﻿#include <pch.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char s1[6] = { 'r','t','k','f','k' };
+	int count = 0;
+	for (int i = 0; i < 6; i++) {
+		if ((s1[i] == 'k') || (s1[i] == 't') || (s1[i] == 'r'))
+		{
+			count++;
+		}
+	}
+	cout << count;
+}
